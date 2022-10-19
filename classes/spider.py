457e34ab17d594c80347785211648f35614db196
Th3R3alDuk3 <60510941+Th3R3alDuk3@users.noreply.github.com>
2022-10-19 23:11:40 +0200

    append scissors

--- classes/spider.py
+++ classes/spider.py
@@ -1,48 +1,62 @@
-import scrapy as scrapy
+from scrapy import Spider, Request
+from classes.scissors import Scissors
 
-from classes.analyzer import Analyzer
 
+class Spiwix(Spider, Scissors):
 
-class Spider(scrapy.Spider):
+    name = "spiwix"
+    # html tags to filter
+    tags = {"h1", "h2", "h3", "h4", "a", "b", "u", "i"}
 
-    name = "spiki"
+    def __init__(self, search_strings: list, languages: list, max_pages: int, **kwargs):
 
-    def __init__(self, languages: list, search_words: list):
-
+        self.search_strings = search_strings
         self.languages = languages
-        self.search_words = search_words
+        self.max_pages = max_pages
+
+        # min_ and max_string_length
+        super().__init__(**kwargs)
 
     def start_requests(self) -> iter:
         for language in self.languages:
-            for search_word in self.search_words:
-                yield scrapy.Request(
+            for search_string in self.search_strings:
+                yield Request(
                     # https://wiki.kiwix.org/wiki/OPDS
-                    f"http://localhost/catalog/search?tag=wikipedia&lang={language}&pattern={search_word}",
-                    callback=self.parse_catalogs
+                    "http://localhost/catalog/search?tag=wikipedia&lang={}&pattern={}".format(
+                        language,
+                        search_string
+                    ), callback=self.parse_catalogs
                 )
 
-    def parse_catalogs(self, response) -> iter:
+    def parse_catalogs(self, response, page_length: int = 50) -> iter:
 
+        # remove all namespaces for better handling
         response.selector.remove_namespaces()
 
         for href in response.xpath("//entry/link[@type='text/html']/@href").getall():
-            for search_word in self.search_words:
-                yield scrapy.Request(
-                    f"http://localhost/search?content={href.strip('/')}&pattern={search_word}",
-                    callback=self.parse_results
-                )
+            for search_string in self.search_strings:
+                # scroll through several pages of a certain length until the maximum is reached
+                for i in range(0, self.max_pages, min(page_length, self.max_pages)):
+                    yield Request(
+                        "http://localhost/search?content={}&start={}&pageLength={}&pattern={}".format(
+                            href.strip("/"),
+                            i + 1, min(i + page_length, self.max_pages),
+                            search_string
+                        ), callback=self.parse_results
+                    )
 
     def parse_results(self, response) -> iter:
         for href in response.xpath("//div[@class='results']//a/@href").getall():
-            yield scrapy.Request(
-                f"http://localhost/{href.strip('/')}",
-                callback=self.parse
+            yield Request(
+                "http://localhost/{}".format(
+                    href.strip('/')
+                ), callback=self.parse
             )
 
     def parse(self, response, **kwargs):
         for div in response.xpath("//div[@id='content']"):
-            for tag in {"h1", "h2", "h3", "h4", "a", "b", "u", "i"}:
+            for tag in self.tags:
                 for text in div.xpath(f"//{tag}/text()").getall():
                     if text:
-                        for string in Analyzer().process(text):
+                        for string in self.process(text):
                             print(string)
--- main.py
+++ main.py
@@ -2,19 +2,23 @@
 
 import argparse
 from scrapy.crawler import CrawlerProcess
-from classes.spider import Spider
+from classes.spider import Spiwix
 
 
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("search_words", nargs="+")
+    parser.add_argument("search_strings", nargs="+")
     parser.add_argument("-l", "--languages", nargs="+", help="ISO-639-3")
+    parser.add_argument("--max_pages", type=int, default=300)
+    parser.add_argument("--min_string_length", type=int, default=3)
+    parser.add_argument("--max_string_length", type=int, default=20)
 
     args = parser.parse_args()
 
     process = CrawlerProcess()
-    process.crawl(Spider, **vars(args))
+    # TODO: require -> search_strings, languages, max_pages, min_string_length, max_string_length
+    process.crawl(Spiwix, **vars(args))
     process.start()
 
 
