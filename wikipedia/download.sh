c509d0668e9a39414fac4884ee86f936e9a3aa9f
Duk3 <duk3@example.org>
2022-10-20 19:55:24 +0200

    code review

--- wikipedia/download.sh
+++ wikipedia/download.sh
@@ -1,4 +1,4 @@
 languages="de en fr"
-for language in $languages do
-    wget --content-disposition "https://download.kiwix.org/zim/wikipedia_${language}_all_nopic.zim"
+for language in $languages; do
+    wget --content-disposition "https://download.kiwix.org/zim/wikipedia_${language}_all_nopic.zim.torrent"
 done
